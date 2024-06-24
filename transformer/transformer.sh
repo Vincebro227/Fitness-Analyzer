63a4621c0f5cb059a6f6f343a6edf12332a25e2f
Vincent Breau <vince.breau@gmail.com>
2024-06-24 13:32:37 -0400

    Added date/time in name of output .txt file.

--- transformer/transformer.sh
+++ transformer/transformer.sh
@@ -13,9 +13,11 @@ DoesFileExist=$(ls *.FIT)
 if [ -n $DoesFileExist ]
 then
 mv $DoesFileExist NewName.FIT
+Date=$(date +%m_%d_%Y_%H_%M)
+fitdump NewName.FIT -t readable > "$Date.txt" 
+cp $(ls *.txt) "../out/"
+rm $(ls *.txt)
 
-fitdump NewName.FIT -t readable > "test.txt" 
-mv test.txt ../out/
 break
 else
 echo wompwompwomp
